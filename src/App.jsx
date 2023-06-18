c811a7653335e2dba15118f336063b563cd0a08a
André Cavalcante <andedis053@gmail.com>
2023-06-18 12:53:05 -0300

    Commit Final

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  
   const [num, setNum] = useState(1);
-  const [nome, setName] = useState("Andre");
-
-  console.log(nome);
+  const [nome, setNome] = useState("Andre");
 
   const changeNum = () => {
     setNum((prevNum) => prevNum + 1);
     setNum((prevNum) => prevNum + 1);
   }
 
-  const changeNome = () => {
-    setNome((nome) => nome)
-  }
-
+  console.log(nome);
+  
   return (
     <>
       <h1>
@@ -24,10 +21,9 @@ function App() {
       <body>
       <h2>Meu numero +2 é: {num}</h2>
       <input type='number' value={num}></input>
-      <button onClick={changeNum} >Mudar número!</button>
+      <button onClick={changeNum}>Mudar número!</button>
       <h2>Meu nome é: {nome}</h2>
       <input type="text" value={nome} onChange = {e =>setNome(e.target.value)}/>
-      <button >Submit!</button>
       </body>
     </>
   )
